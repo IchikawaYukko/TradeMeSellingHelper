6c26dd24ef747387c826db334ab1713e434633a3
市川ゆり子/IchikawaYuriko <ichikawayuriko@yahoo.co.jp>
2018-02-06 16:01:23 +1300

    initial import from local

---
+++ TradeMeMarketPlace自動入力.js
@@ -0,0 +1,133 @@
+//TradeMe MarketPlace商品説明送料自動入力
+
+//アイアン
+javascript: (function() {
+if(document.querySelectorAll('input[name="IsUnlimitedStock"]').item(0).checked == true) document.querySelectorAll('input[name="IsUnlimitedStock"]').item(0).click();
+document.querySelectorAll('input[name="Product.Literal.WarehouseStock"]').item(0).value = 1;
+
+document.querySelectorAll('textarea#TradeMeListingRule_Description').item(0).value = "shaft: \nflex: \n*set(5～9,P)\n";
+
+document.querySelectorAll('input#TradeMeListingRule_IsListingFooterEnabled').item(0).checked = true;
+document.querySelectorAll('input#TradeMeListingRule_IsListingNew').item(0).checked = false;
+
+if(document.querySelectorAll('input#TradeMeListingRule_FixedPriceOfferAutoEnabled').item(0).checked == true) document.querySelectorAll('input#TradeMeListingRule_FixedPriceOfferAutoEnabled').item(0).click();
+
+if(document.querySelectorAll('input#TradeMeListingRule_IsPaymentOther').item(0).checked == false) document.querySelectorAll('input#TradeMeListingRule_IsPaymentOther').item(0).click();
+document.querySelectorAll('input#TradeMeListingRule_PaymentOther').item(0).value = "EFTPOS, Visa, MasterCard";
+
+document.querySelectorAll('select#TradeMeListingRule_IsShippingPricePerQuantity').item(0).selectedIndex = 0;
+
+if(document.querySelectorAll('input#TradeMeListingRule_IncludeInCombinedShipping').item(0).checked == true) document.querySelectorAll('input#TradeMeListingRule_IncludeInCombinedShipping').item(0).click();
+document.querySelectorAll('input#autolist-on').item(0).checked = true;
+})();
+
+-----------
+○パター
+javascript: (function() {
+if(document.querySelectorAll('input[name="IsUnlimitedStock"]').item(0).checked == true) document.querySelectorAll('input[name="IsUnlimitedStock"]').item(0).click();
+document.querySelectorAll('input[name="Product.Literal.WarehouseStock"]').item(0).value = 1;
+
+document.querySelectorAll('textarea#TradeMeListingRule_Description').item(0).value = "shaft: \nlength: \n\nHead cover include　又は　Head cover NOT include";
+
+document.querySelectorAll('input#TradeMeListingRule_IsListingFooterEnabled').item(0).checked = true;
+document.querySelectorAll('input#TradeMeListingRule_IsListingNew').item(0).checked = false;
+
+if(document.querySelectorAll('input#TradeMeListingRule_FixedPriceOfferAutoEnabled').item(0).checked == true) document.querySelectorAll('input#TradeMeListingRule_FixedPriceOfferAutoEnabled').item(0).click();
+
+if(document.querySelectorAll('input#TradeMeListingRule_IsPaymentOther').item(0).checked == false) document.querySelectorAll('input#TradeMeListingRule_IsPaymentOther').item(0).click();
+document.querySelectorAll('input#TradeMeListingRule_PaymentOther').item(0).value = "EFTPOS, Visa, MasterCard";
+
+document.querySelectorAll('select#TradeMeListingRule_IsShippingPricePerQuantity').item(0).selectedIndex = 0;
+
+if(document.querySelectorAll('input#TradeMeListingRule_IncludeInCombinedShipping').item(0).checked == true) document.querySelectorAll('input#TradeMeListingRule_IncludeInCombinedShipping').item(0).click();
+document.querySelectorAll('input#autolist-on').item(0).checked = true;
+})();
+
+-----------
+○ドライバー、ウッド、UT
+javascript: (function() {
+if(document.querySelectorAll('input[name="IsUnlimitedStock"]').item(0).checked == true) document.querySelectorAll('input[name="IsUnlimitedStock"]').item(0).click();
+document.querySelectorAll('input[name="Product.Literal.WarehouseStock"]').item(0).value = 1;
+
+document.querySelectorAll('textarea#TradeMeListingRule_Description').item(0).value = "shaft: \nflex: \nloft: °\n\nHead cover included 　又は　Head cover NOT included";
+
+document.querySelectorAll('input#TradeMeListingRule_IsListingFooterEnabled').item(0).checked = true;
+document.querySelectorAll('input#TradeMeListingRule_IsListingNew').item(0).checked = false;
+
+if(document.querySelectorAll('input#TradeMeListingRule_FixedPriceOfferAutoEnabled').item(0).checked == true) document.querySelectorAll('input#TradeMeListingRule_FixedPriceOfferAutoEnabled').item(0).click();
+
+if(document.querySelectorAll('input#TradeMeListingRule_IsPaymentOther').item(0).checked == false) document.querySelectorAll('input#TradeMeListingRule_IsPaymentOther').item(0).click();
+document.querySelectorAll('input#TradeMeListingRule_PaymentOther').item(0).value = "EFTPOS, Visa, MasterCard";
+
+document.querySelectorAll('select#TradeMeListingRule_IsShippingPricePerQuantity').item(0).selectedIndex = 0;
+
+if(document.querySelectorAll('input#TradeMeListingRule_IncludeInCombinedShipping').item(0).checked == true) document.querySelectorAll('input#TradeMeListingRule_IncludeInCombinedShipping').item(0).click();
+document.querySelectorAll('input#autolist-on').item(0).checked = true;
+})();
+
+-----------
+○ウェッジ
+javascript: (function() {
+if(document.querySelectorAll('input[name="IsUnlimitedStock"]').item(0).checked == true) document.querySelectorAll('input[name="IsUnlimitedStock"]').item(0).click();
+document.querySelectorAll('input[name="Product.Literal.WarehouseStock"]').item(0).value = 1;
+
+document.querySelectorAll('textarea#TradeMeListingRule_Description').item(0).value = "shaft: \nloft: °";
+
+document.querySelectorAll('input#TradeMeListingRule_IsListingFooterEnabled').item(0).checked = true;
+document.querySelectorAll('input#TradeMeListingRule_IsListingNew').item(0).checked = false;
+
+if(document.querySelectorAll('input#TradeMeListingRule_FixedPriceOfferAutoEnabled').item(0).checked == true) document.querySelectorAll('input#TradeMeListingRule_FixedPriceOfferAutoEnabled').item(0).click();
+
+if(document.querySelectorAll('input#TradeMeListingRule_IsPaymentOther').item(0).checked == false) document.querySelectorAll('input#TradeMeListingRule_IsPaymentOther').item(0).click();
+document.querySelectorAll('input#TradeMeListingRule_PaymentOther').item(0).value = "EFTPOS, Visa, MasterCard";
+
+document.querySelectorAll('select#TradeMeListingRule_IsShippingPricePerQuantity').item(0).selectedIndex = 0;
+
+if(document.querySelectorAll('input#TradeMeListingRule_IncludeInCombinedShipping').item(0).checked == true) document.querySelectorAll('input#TradeMeListingRule_IncludeInCombinedShipping').item(0).click();
+document.querySelectorAll('input#autolist-on').item(0).checked = true;
+})();
+
+-----------
+○バッグ
+javascript: (function() {
+if(document.querySelectorAll('input[name="IsUnlimitedStock"]').item(0).checked == true) document.querySelectorAll('input[name="IsUnlimitedStock"]').item(0).click();
+document.querySelectorAll('input[name="Product.Literal.WarehouseStock"]').item(0).value = 1;
+
+document.querySelectorAll('textarea#TradeMeListingRule_Description').item(0).value = "*BRAND NEW*\n名前\n\nModel: \nColour: ";
+
+document.querySelectorAll('input#TradeMeListingRule_IsListingFooterEnabled').item(0).checked = true;
+document.querySelectorAll('input#TradeMeListingRule_IsListingNew').item(0).checked = true;
+
+if(document.querySelectorAll('input#TradeMeListingRule_FixedPriceOfferAutoEnabled').item(0).checked == true) document.querySelectorAll('input#TradeMeListingRule_FixedPriceOfferAutoEnabled').item(0).click();
+
+if(document.querySelectorAll('input#TradeMeListingRule_IsPaymentOther').item(0).checked == false) document.querySelectorAll('input#TradeMeListingRule_IsPaymentOther').item(0).click();
+document.querySelectorAll('input#TradeMeListingRule_PaymentOther').item(0).value = "EFTPOS, Visa, MasterCard";
+
+document.querySelectorAll('select#TradeMeListingRule_IsShippingPricePerQuantity').item(0).selectedIndex = 0;
+
+if(document.querySelectorAll('input#TradeMeListingRule_IncludeInCombinedShipping').item(0).checked == true) document.querySelectorAll('input#TradeMeListingRule_IncludeInCombinedShipping').item(0).click();
+document.querySelectorAll('input#autolist-on').item(0).checked = true;
+})();
+
+送料自動入力(カート)
+-----------
+javascript: (function() {
+if(document.querySelectorAll('input[name="IsUnlimitedStock"]').item(0).checked == true) document.querySelectorAll('input[name="IsUnlimitedStock"]').item(0).click();
+document.querySelectorAll('input[name="Product.Literal.WarehouseStock"]').item(0).value = 1;
+
+document.querySelectorAll('textarea#TradeMeListingRule_Description').item(0).value = "Trundler\nRovic RV2L\nColour: **/**\n\nThis is a brand new compact push cart.\nIt's quick and simple to use with one action folding and unfolding.\nIt has console for tees, balls, a score card and a drink.\nIt also comes with an umbrella holder and  a foot brake.";
+
+document.querySelectorAll('input#TradeMeListingRule_IsListingFooterEnabled').item(0).checked = true;
+document.querySelectorAll('input#TradeMeListingRule_IsListingNew').item(0).checked = true;
+
+if(document.querySelectorAll('input#TradeMeListingRule_FixedPriceOfferAutoEnabled').item(0).checked == true) document.querySelectorAll('input#TradeMeListingRule_FixedPriceOfferAutoEnabled').item(0).click();
+
+if(document.querySelectorAll('input#TradeMeListingRule_IsPaymentOther').item(0).checked == false) document.querySelectorAll('input#TradeMeListingRule_IsPaymentOther').item(0).click();
+document.querySelectorAll('input#TradeMeListingRule_PaymentOther').item(0).value = "EFTPOS, Visa, MasterCard";
+
+document.querySelectorAll('select#TradeMeListingRule_IsShippingPricePerQuantity').item(0).selectedIndex = 0;
+
+if(document.querySelectorAll('input#TradeMeListingRule_IncludeInCombinedShipping').item(0).checked == true) document.querySelectorAll('input#TradeMeListingRule_IncludeInCombinedShipping').item(0).click();
+document.querySelectorAll('input#autolist-on').item(0).checked = true;
+})();
+
